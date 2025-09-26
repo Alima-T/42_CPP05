218e3bf0ba457fd7ffd7f2ec7f632b5a1a207b0c
Alima-T <okhapka.a@gmail.com>
2025-09-27 00:55:03 +0200

    add PresidentialPardonForm files

---
+++ ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/27 00:15:55 by aokhapki          #+#    #+#             */
+/*   Updated: 2025/09/27 00:17:57 by aokhapki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+#include <string>
+
+class PresidentialPardonForm : public AForm 
+{
+private:
+    std::string m_target;
+
+public:
+    PresidentialPardonForm(const std::string &target);
+    PresidentialPardonForm(const PresidentialPardonForm &src);
+    PresidentialPardonForm &operator=(const PresidentialPardonForm &rhs);
+    ~PresidentialPardonForm();
+
+    void execute(Bureaucrat const &executor) const;
+};
+
+#endif
